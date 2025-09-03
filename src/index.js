201920886eb9d4fc1cfcaf501f1ebb35e3cca701
fardin04 <fardinhasan.mayen@gmail.com>
2025-09-03 23:28:20 +0600

    add express cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenve from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 
@@ -8,5 +9,13 @@ dotenve.config({
     path:"./env"
 })
 
-connectDB();
+connectDB().then(() => {
+    console.log("Database connection successful");
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+}).catch((error) => {
+    console.error("Database connection Failed:", error);
+    process.exit(1);
+});
 
