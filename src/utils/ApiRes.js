ee6390f5ec65d71ff2128d1bffa4d469898b390f
fardin04 <fardinhasan.mayen@gmail.com>
2025-09-04 00:32:24 +0600

    add utils

---
+++ src/utils/ApiRes.js
@@ -0,0 +1,10 @@
+class ApiRes {
+    constructor(statusCode,data, message="Success") {
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+        this.success = statusCode >= 100 && statusCode < 400;
+    }
+}
+
+export { ApiRes };
