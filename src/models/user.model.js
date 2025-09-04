82a216d71acb90825ca5dfec9e5098f6206806f3
fardin04 <fardinhasan.mayen@gmail.com>
2025-09-04 14:18:34 +0600

    Add user and video models with authentication and pagination features

---
+++ src/models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true, lowercase: true, trim: true, index: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true,},
+    fullName: { type: String, required: true, trim: true,index: true },
+    password: { type: String, required: true },
+    avatar: { type: String,required:true, default: null },
+    coverImage: { type: String, default: null },
+    watchHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Video' }],
+    refreshToken: { type: String },
+
+}, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// generate access token
+
+userSchema.methods.generateAccessToken = function() {
+  jwt.sign({
+    id: this._id,
+    username: this.username,
+    email: this.email
+  }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRATION }, (err, token) => {
+    if (err) {
+      throw new Error('Error generating access token');
+    }
+    return token;
+  });   
+}
+
+// generate refresh token
+userSchema.methods.generateRefreshToken = function() {
+  jwt.sign({
+    id: this._id,
+    username: this.username,
+    email: this.email
+  }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRATION }, (err, token) => {
+    if (err) {
+      throw new Error('Error generating refresh token');
+    }
+    return token;
+  });
+}
+
+export const User = mongoose.model("User", userSchema);
---
+++ src/models/video.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new mongoose.Schema({
+    videoFile: { type: String, required: true },
+    title: { type: String, required: true, trim: true, index: true },
+    description: { type: String, default: null, trim: true },
+    videoUrl: { type: String, required: true },
+    thumbnail: { type: String, default: null },
+    views: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
+    dislikes: { type: Number, default: 0 },
+    duration: { type: Number, required: true }, // duration in seconds
+    uploadedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tags: [{ type: String, trim: true, index: true }],
+    isPublished: { type: Boolean, default: true },
+    commentsEnabled: { type: Boolean, default: true },
+}, { timestamps: true });
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
